f365e211ebf1e731e4c8430c3724da0ed2aeb404
Matyas Kone <bikesvatera@gmail.com>
2021-04-06 13:15:13 +0200

    Multiple Images can be picked from the gallery
    Dependency hell is fixed, build runner is fixed
    
    lint error fixed

---
+++ lib/delegates/paperless_text_delegate.dart
@@ -0,0 +1,40 @@
+import 'package:wechat_assets_picker/wechat_assets_picker.dart';
+import 'package:paperless_app/i18n.dart';
+
+class PaperlessAssetsPickerTextDelegate implements AssetsPickerTextDelegate {
+  @override
+  String cancel = 'Cancel'.i18n;
+
+  @override
+  String confirm = 'Confirm'.i18n;
+
+  @override
+  String edit = 'Edit'.i18n;
+
+  @override
+  String gifIndicator = 'Gif'.i18n;
+
+  @override
+  String heicNotSupported = 'Not supported'.i18n;
+
+  @override
+  String loadFailed = 'Failed to load'.i18n;
+
+  @override
+  String original = 'Original'.i18n;
+
+  @override
+  String preview = 'Preview'.i18n;
+
+  @override
+  String select = 'Select'.i18n;
+
+  @override
+  String unSupportedAssetType = 'Not supported type'.i18n;
+
+  @override
+  String durationIndicatorBuilder(Duration duration) {
+    // TODO: implement durationIndicatorBuilder
+    throw UnimplementedError();
+  }
+}

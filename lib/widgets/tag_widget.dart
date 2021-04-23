cd62ca6a988f3e1784881481235abbf34408adde
Johann Bauer <bauerj@bauerj.eu>
2021-04-23 16:00:52 +0200

    Improve Tag widget appearance

--- lib/widgets/tag_widget.dart
+++ lib/widgets/tag_widget.dart
@@ -24,15 +24,36 @@ class TagWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Text(tag.name,
-        textAlign: TextAlign.right,
-        style: TextStyle(
-          backgroundColor: getColor(),
-        ));
+    return Container(
+      decoration: new BoxDecoration(borderRadius: BorderRadius.all(Radius.circular(5)), color: getColor()),
+
+      child: Padding(
+        padding: EdgeInsets.symmetric(vertical: 4, horizontal: 10),
+        child: Row(children: [
+          Icon(
+            Icons.label_important_outline,
+            size: 17,
+            color: getTextColor()
+          ),
+          Text(
+            tag.name,
+            textAlign: TextAlign.right,
+            style: TextStyle(color: getTextColor()
+            )
+          )
+        ]),
+      ),
+    );
   }
 
   Color getColor() {
     return _fromHex(tag.colourCode);
+  }
+
+  Color getTextColor() {
+    if (ThemeData.estimateBrightnessForColor(getColor()) == Brightness.light)
+      return Colors.black;
+    return Colors.white;
   }
 
   // https://stackoverflow.com/a/50081214/1024057

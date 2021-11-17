ab10068653d9bf3094a4f1c431d57cd639bc280f
Johann Bauer <bauerj@bauerj.eu>
2021-11-17 14:26:19 +0100

    Add AppBar to settings view

--- lib/routes/settings_route.dart
+++ lib/routes/settings_route.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:paperless_app/i18n.dart';
 import 'package:settings_ui/settings_ui.dart';
 import 'package:shared_preferences/shared_preferences.dart';
-import 'package:paperless_app/i18n.dart';
 
 final _scaffoldKey = GlobalKey<ScaffoldState>();
 
@@ -33,14 +33,19 @@ class _SettingsRouteState extends State<SettingsRoute> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        title: Text("Settings".i18n),
+      ),
       key: _scaffoldKey,
       body: SettingsList(
+        contentPadding: EdgeInsets.fromLTRB(5, 20, 5, 0),
         sections: [
           SettingsSection(
             title: 'View'.i18n,
             tiles: [
               SettingsTile.switchTile(
                 title: 'Invert Document Preview in Dark Mode'.i18n,
+                titleMaxLines: 2,
                 leading: Icon(Icons.invert_colors),
                 switchValue: invertDocumentPreview,
                 onToggle: (bool value) {
